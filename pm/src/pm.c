4d2d9aa8483e4eae6e5829e4a858c1e2f7d6dd6a
Mohammad Amin Mollazadeh <madamin@pm.me>
2021-02-12 16:05:36 +0330

    Check arguments in PackageManager

--- pm/src/pm.c
+++ pm/src/pm.c
@@ -1,6 +1,51 @@
 #include<stdio.h>
+#include<string.h>
+
+void print_usage() {
+	printf("Usage: PackageManager run <package> [command]\
+\t\t info <package>\
+\t\t list\
+\t\t install <file>\
+\t\t uninstall <package>\n");
+}
 
 int main(int argc, char **args) {
-	printf("PackageManager is not implemented");
-	return 0;
+	if (argc <= 1) {
+		fprintf(stderr, "No command specified.\n");
+		print_usage();
+		return 1;
+	}
+
+	if (strcmp(args[1], "help") == 0 ||
+		strcmp(args[1], "--help") == 0 ||
+		strcmp(args[1], "-help") == 0 ||
+		strcmp(args[1], "-h") == 0 ||
+		strcmp(args[1], "-?") == 0) {
+		print_usage();
+		return 0;
+	}
+
+	if (strcmp(args[1], "list") == 0) {
+		printf("PackageManager-list is not implemented\n");
+		return 2;
+	}
+
+	if (strcmp(args[1], "run") == 0) {
+		printf("PackageManager-run is not implemented\n");
+		return 2;
+	}
+
+	if (strcmp(args[1], "install") == 0) {
+		printf("PackageManager-install is not implemented\n");
+		return 0;
+	}
+
+	if (strcmp(args[1], "uninstall") == 0) {
+		printf("PackageManager-uninstall is not implemented\n");
+		return 0;
+	}
+
+	fprintf(stderr, "Invalid command: %s\n", args[1]);
+	print_usage();
+	return 1;
 }
